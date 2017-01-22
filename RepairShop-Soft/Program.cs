40b3a36d97139bc30003b05f8227103d2f035238
Dr.Ehsan Shaghaei <ehsan2754.o@outlook.com>
2017-01-23 01:37:48 +0330

    Databases And Tables Created #2

--- RepairShop-Soft/Program.cs
+++ RepairShop-Soft/Program.cs
@@ -16,7 +16,7 @@ namespace RepairShop_Soft
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new main_form());
         }
     }
 }
--- RepairShop-Soft/main_form.cs
+++ RepairShop-Soft/main_form.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace RepairShop_Soft
 {
-    public partial class Form1 : Form
+    public partial class main_form : Form
     {
-        public Form1()
+        public main_form()
         {
             InitializeComponent();
         }
