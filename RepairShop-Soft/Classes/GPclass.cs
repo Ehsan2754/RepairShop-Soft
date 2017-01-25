fbb97990418d917aa0ed8abf9bf37c2f09da1082
Dr.Ehsan Shaghaei <ehsan2754.o@outlook.com>
2017-01-26 00:36:26 +0330

    #the main form prepration

---
+++ RepairShop-Soft/Classes/GPclass.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+public class GPclass
+{
+    //generall - perpose class for project
+    public static string userbar { get; set; }
+    public static System.Globalization.PersianCalendar calendar = new System.Globalization.PersianCalendar();
+    public GPclass()
+	{
+	}
+    public static string getDate ()
+    {
+        string output = calendar.GetYear(DateTime.Now) + "/" + calendar.GetMonth(DateTime.Now) + "/" + calendar.GetDayOfMonth(DateTime.Now);
+        return output;
+    }
+    public static string getHour()
+    {
+        
+        string output = calendar.GetHour(DateTime.Now) + ":" + calendar.GetMinute(DateTime.Now) + ":" + calendar.GetSecond(DateTime.Now);
+        return output;
+    }
+}
--- RepairShop-Soft/main_form.cs
+++ RepairShop-Soft/main_form.cs
@@ -16,5 +16,22 @@ namespace RepairShop_Soft
         {
             InitializeComponent();
         }
+
+        private void main_form_Load(object sender, EventArgs e)
+        {
+            GPclass.userbar = "ناشناخته";
+            this.RightToLeft = RightToLeft.Yes;
+            this.Text = "مدیریت تعمیرگاه       کاربر     :" + GPclass.userbar;
+            lbl_userNOT.Text = GPclass.userbar;
+            lbl_date.Text = GPclass.getDate();
+            lbl_hour.Text = DateTime.Now.ToShortTimeString();
+            lbl_day.Text = GPclass.calendar.GetDayOfWeek(DateTime.Now).ToString();
+            
+        }
+
+        private void groupBox1_Enter(object sender, EventArgs e)
+        {
+           
+        }
     }
 }
