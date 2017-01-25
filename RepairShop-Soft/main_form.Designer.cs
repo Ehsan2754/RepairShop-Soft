fd60f195caae38f98d4bc5f7b8409e8cbc66ec5a
Dr.Ehsan Shaghaei <ehsan2754.o@outlook.com>
2017-01-26 00:36:26 +0330

    #the main form prepration

--- RepairShop-Soft/main_form.Designer.cs
+++ RepairShop-Soft/main_form.Designer.cs
@@ -28,20 +28,89 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(main_form));
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.lbl_userNOT = new System.Windows.Forms.Label();
+            this.lbl_date = new System.Windows.Forms.Label();
+            this.lbl_hour = new System.Windows.Forms.Label();
+            this.lbl_day = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.lbl_day);
+            this.groupBox1.Controls.Add(this.lbl_hour);
+            this.groupBox1.Controls.Add(this.lbl_date);
+            this.groupBox1.Controls.Add(this.lbl_userNOT);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(106, 109);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "اعلانات";
+            this.groupBox1.Enter += new System.EventHandler(this.groupBox1_Enter);
+            // 
+            // lbl_userNOT
+            // 
+            this.lbl_userNOT.AutoSize = true;
+            this.lbl_userNOT.Location = new System.Drawing.Point(12, 16);
+            this.lbl_userNOT.Name = "lbl_userNOT";
+            this.lbl_userNOT.Size = new System.Drawing.Size(48, 13);
+            this.lbl_userNOT.TabIndex = 1;
+            this.lbl_userNOT.Text = "ناشناخته";
+            // 
+            // lbl_date
+            // 
+            this.lbl_date.AutoSize = true;
+            this.lbl_date.Location = new System.Drawing.Point(17, 39);
+            this.lbl_date.Name = "lbl_date";
+            this.lbl_date.Size = new System.Drawing.Size(32, 13);
+            this.lbl_date.TabIndex = 1;
+            this.lbl_date.Text = "تاریخ";
+            // 
+            // lbl_hour
+            // 
+            this.lbl_hour.AutoSize = true;
+            this.lbl_hour.Location = new System.Drawing.Point(16, 62);
+            this.lbl_hour.Name = "lbl_hour";
+            this.lbl_hour.Size = new System.Drawing.Size(35, 13);
+            this.lbl_hour.TabIndex = 1;
+            this.lbl_hour.Text = "ساعت";
+            // 
+            // lbl_day
+            // 
+            this.lbl_day.AutoSize = true;
+            this.lbl_day.Location = new System.Drawing.Point(16, 84);
+            this.lbl_day.Name = "lbl_day";
+            this.lbl_day.Size = new System.Drawing.Size(23, 13);
+            this.lbl_day.TabIndex = 1;
+            this.lbl_day.Text = "روز";
             // 
             // main_form
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.ClientSize = new System.Drawing.Size(684, 562);
+            this.Controls.Add(this.groupBox1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "main_form";
+            this.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
             this.Text = "MainForm";
+            this.Load += new System.EventHandler(this.main_form_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label lbl_userNOT;
+        private System.Windows.Forms.Label lbl_date;
+        private System.Windows.Forms.Label lbl_hour;
+        private System.Windows.Forms.Label lbl_day;
     }
 }
 
